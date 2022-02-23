e51f886149cb9b33a10be98ded7616bb859ecebe
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-02-23 17:52:06 -0300

    11 | Válida e envia reserva para BDD*

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var menus = require('../inc/menus')
 var router = express.Router();
+var reservations = require('../inc/reservations')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -33,14 +34,32 @@ router.get('/menus', function(req, res, next) {
 });
 
 router.get('/reservations', function(req, res, next) {
-  res.render('reservations',  {
-    title: 'Reservas - Restaurante Saboroso',
-    background: 'images/img_bg_2.jpg',
-    heading: 'Reserve uma Mesa!'
-  });
+  reservations.render(req, res);
+});
+
+router.post('/reservations', function(req, res, next) {
+  if(!req.body.name) {
+    reservations.render(req, res, 'Digite o nome!');
+  } else if(!req.body.email) {
+    reservations.render(req, res, 'Digite um e-mail para contato!');
+  } else if(!req.body.people) {
+    reservations.render(req, res, 'Selecione a quantidade de pessoas!');
+  } else if(!req.body.date) {
+    reservations.render(req, res, 'Selecione a data da reserva!');
+  } else if(!req.body.time) {
+    reservations.render(req, res, 'Selecione a hora da reserva!')
+  } else {
+    reservations.save(req.body).then(results => {
+      req.body = {}
+      reservations.render(req, res, null, 'Reserva realizada com sucesso!')
+    }).catch(err => {
+      reservations.render(req, res, err.message);
+    });
+  }
 });
 
 router.get('/services', function(req, res, next) {
+
   res.render('services', {
     title: 'Serviços - Restaurante Saboroso',
     background: 'images/img_bg_1.jpg',
