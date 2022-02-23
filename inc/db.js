847e3ddd6b540f96cd10888612dd3286d7d28987
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-02-23 14:33:00 -0300

    04 | Conecta Nodejs com o MySQL

---
+++ inc/db.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    database: 'saboroso',
+    password: 'Clear.Rod22'
+});
+
+module.exports = connection;
