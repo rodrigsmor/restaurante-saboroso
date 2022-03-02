f2c2ccaecfcc7672e67ff4c31f531d88c5ec1305
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-02 12:22:30 -0300

    49 | Instala e configura Web Socket.io

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ var logger = require('morgan');
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
 var formidable = require('formidable');
-
+var http = require('http');
+var socket = require('socket.io');
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 
 var app = express();
+
+var http = http.Server(app);
+var io = socket(http);
+
+io.on('connection', function(socket) {
+  console.log('Novo usuário conectado.')
+})
 
 app.use(function(req, res, next) {
   if(req.method === 'POST') {
@@ -46,7 +54,6 @@ app.use(session({
 )
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -69,4 +76,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+http.listen(3000, function() {
+  console.log('Servidor em execução...')
+});
