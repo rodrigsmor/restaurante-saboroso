e2f5724cf6931c0bbdcf29523d5603b57ade81b7
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-02 10:35:58 -0300

    46 | Implementa os botões de retornar e prosseguir

--- inc/Pagination.js
+++ inc/Pagination.js
@@ -68,11 +68,25 @@ class Pagination {
             nrend = this.getCurrentPage() + parseInt(limitPagesNav / 2)
         }
 
+        if(this.getCurrentPage() > 1) {
+            links.push({
+                text: '<',
+                href: '?' + this.getQueryString(Object.assign({}, params, {page: this.getCurrentPage() - 1})),
+            });
+        }
+        
         for (let x = nrstart; x <= nrend; x++) {
             links.push({
                 text: x,
                 href: '?' + this.getQueryString(Object.assign({}, params, {page: x})),
                 active: (x === this.getCurrentPage())
+            });
+        }
+
+        if(this.getCurrentPage() < this.getTotalPages()) {
+            links.push({
+                text: '>',
+                href: '?' + this.getQueryString(Object.assign({}, params, {page: this.getCurrentPage() + 1})),
             });
         }
 
